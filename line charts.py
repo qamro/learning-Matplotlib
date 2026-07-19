7ae1dd4f265eeb241d6cf665a8acddcab97388db
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-19 19:09:22 +0200

    line charts

--- line charts.py
+++ line charts.py
@@ -1,3 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = [2023, 2024, 2025, 2026]
+x = np.array([2023, 2024, 2025, 2026])
+y = np.array([3, 15, 28, 31])
+plt.plot(x, y)
+plt.show()
+
