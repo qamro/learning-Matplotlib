7b64a5a54c1473adff08807e19f454d79513ea5d
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-19 19:34:43 +0200

    plot customization

--- plot customization.py
+++ plot customization.py
@@ -5,43 +5,50 @@ x = np.array([2023, 2024, 2025, 2026])
 y = np.array([3, 15, 28, 31])
 plt.plot(x, y, marker=".",    # customize the marker
                 markersize=20,     # customize the marker size
-                markerfacecolor="Red",   # customize the color of the marker by the name of the color directly or the Hexadecimal value like #1cd3fc
-                )    
+                markerfacecolor="Red",   # customize the color of the marker 
+                markeredgecolor="Green",
+                )   # customize the color of the edges(borders) of the marker  
+plt.show()
+
+
+
+
 """
 HERE ARE ALL THE MARKERS: 
 
 Points/dots
 
-. — point
-, — pixel
+. : point
+, : pixel
 
 Basic shapes
 
-o — circle
-s — square
-D — diamond
-d — thin diamond
-p — pentagon
-h — hexagon1
-H — hexagon2
-8 — octagon
+o : circle
+s : square
+D : diamond
+d : thin diamond
+p : pentagon
+h : hexagon1
+H : hexagon2
+8 : octagon
 
 Triangles
 
-v — triangle down
-^ — triangle up
-< — triangle left
-> — triangle right
-1 — tri_down
-2 — tri_up
-3 — tri_left
-4 — tri_right
+v : triangle down
+^ : triangle up
+< : triangle left
+> : triangle right
+1 : tri_down
+2 : tri_up
+3 : tri_left
+4 : tri_right
 
 Lines/crosses
 
-+ — plus
-x — x
-X — filled x
-| — vertical line
-_ — horizontal lin
++ : plus
+x : x
+X : filled x
+| : vertical line
+_ : horizontal line
+
 """
