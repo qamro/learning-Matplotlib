bd69bc42d2e0e912cc69d7b1e3d88a093cb90deb
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-24 03:39:52 +0200

    customization

--- plot customization.py
+++ plot customization.py
@@ -6,30 +6,30 @@ y1 = np.array([3, 15, 28, 31])
 y2 = np.array([5, 32, 15, 7])
 y3 = np.array([25, 12, 40, 2])
 plt.plot(x, y1, marker=".",    # customize the marker
-                markersize=30,     # customize the marker size
+                markersize=15,     # customize the marker size
                 markerfacecolor="Red",   # customize the color of the marker 
                 markeredgecolor="Green",  # customize the color of the edges(borders) of the marker
                 linestyle="solid",  # customize the line style of the plot
-                linewidth=4,  # customize the line width of the plot(its linewidth=1 by default btw)
+                linewidth=3,  # customize the line width of the plot(its linewidth=1 by default btw)
                 color="Grey")  # customize the color of the line plot
 
 # NOTE: we can use multiple plots in the same figure
 # customize a second plot line in the same figure with the first plot
 plt.plot(x, y2, marker=".",    # customize the marker
-                markersize=30,     # customize the marker size
+                markersize=15,     # customize the marker size
                 markerfacecolor="Red",   # customize the color of the marker 
                 markeredgecolor="Green",  # customize the color of the edges(borders) of the marker
                 linestyle="solid",  # customize the line style of the plot
-                linewidth=4,  # customize the line width of the plot(its linewidth=1 by default btw)
+                linewidth=3,  # customize the line width of the plot(its linewidth=1 by default btw)
                 color="Grey")  # customize the color of the line plot)
 
 # customize a third plot line in the same figure with the first and the second plots
 plt.plot(x, y3, marker=".",    # customize the marker
-                markersize=30,     # customize the marker size
+                markersize=15,     # customize the marker size
                 markerfacecolor="Red",   # customize the color of the marker 
                 markeredgecolor="Green",  # customize the color of the edges(borders) of the marker
                 linestyle="solid",  # customize the line style of the plot
-                linewidth=4,  # customize the line width of the plot(its linewidth=1 by default btw)
+                linewidth=3,  # customize the line width of the plot(its linewidth=1 by default btw)
                 color="Grey")  # customize the color of the line plot)
 plt.show()
 
