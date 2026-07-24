f0ba3fa8ae2a9900793b04432b0d43bac8f7ea82
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-24 04:13:28 +0200

    labels

--- labels.py
+++ labels.py
@@ -5,3 +5,9 @@ x = np.array([2023, 2024, 2025, 2026])
 y1 = np.array([3, 15, 28, 31])
 y2 = np.array([5, 32, 15, 7])
 y3 = np.array([25, 12, 40, 2])
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.show()
