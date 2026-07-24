1a43b3d457d2dba097d296e1672887f7cc3c0737
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-24 04:15:21 +0200

    labels

--- labels.py
+++ labels.py
@@ -6,6 +6,9 @@ y1 = np.array([3, 15, 28, 31])
 y2 = np.array([5, 32, 15, 7])
 y3 = np.array([25, 12, 40, 2])
 
+# customize the title of the plots
+plt.title("Class size")
+
 plt.plot(x, y1)
 plt.plot(x, y2)
 plt.plot(x, y3)
