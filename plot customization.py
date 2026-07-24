84a5b9ccc856b44ff0d58369936b73e0bba7a111
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-24 03:36:31 +0200

    customization

--- plot customization.py
+++ plot customization.py
@@ -4,6 +4,7 @@ import numpy as np
 x = np.array([2023, 2024, 2025, 2026])
 y1 = np.array([3, 15, 28, 31])
 y2 = np.array([5, 32, 15, 7])
+y3 = np.array([5, 32, 15, 7])
 plt.plot(x, y1, marker=".",    # customize the marker
                 markersize=30,     # customize the marker size
                 markerfacecolor="Red",   # customize the color of the marker 
