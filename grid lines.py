e60a7200a631840303715e04ce2263c3003b6cb4
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-26 03:14:45 +0200

    grid lines customization

--- grid lines.py
+++ grid lines.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np  
 
 # plt.grid() make the plots easier to read by adding reference lines(grid lines)
-# we can use just plt.grid() to create grid lines or you can customize them by adding arguments to the grid() function
+# we can use just plt.grid() to create simple grid lines or you can customize them by adding arguments to the grid() function
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([5, 10, 15, 20, 25])
