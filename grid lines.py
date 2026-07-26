618ddf52eeedda819cb45a54a9da5d9d01007e3c
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-26 03:22:00 +0200

    grid lines customization

--- grid lines.py
+++ grid lines.py
@@ -6,6 +6,12 @@ import numpy as np
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([5, 10, 15, 20, 25])
-plt.grid()
+
+# customize the grid lines
+plt.grid(axis="x", # here we select the axis that we want to be shown and customized(we can write:axis="x", axis="y" or axis="both") 
+        linewidth=2, # customize the linewidth of the grid lines (its 1 by default)
+        linestyle="dashed", # customize the linestyle of the grid lines
+        color="Green") # customize the color of the grid lines
+
 plt.plot(x, y)
 plt.show()
