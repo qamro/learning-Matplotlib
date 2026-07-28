dcf0caed075a447d3b9ad6a9a7507939ec13a148
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-28 02:43:23 +0200

    pie charts

--- pie charts.py
+++ pie charts.py
@@ -1,3 +1,5 @@
 import matplotlib.pyplot as plt       
 import numpy as np 
 
+categories = ["web developers", "mobile developers", "DevOps engineers", "AI/ML enginners"]
+values = [432, 210, 150, 298]
