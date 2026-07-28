45da7e8cbc74a4f7291da2e478de10a7a38201be
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-28 03:36:45 +0200

    scatter plots

--- scatter plots.py
+++ scatter plots.py
@@ -13,14 +13,18 @@ y2 = np.array([2, 5, 7, 7.5, 10, 12, 13, 16, 18, 20]) # Grades of Class B
 # the first scatter plot
 plt.scatter(x1, y1, color="orange", # customize the color of the dots in scatter plot
                     alpha=0.5, # customize the transparency of the color of the dots in scatter plot
-                    s=100) # customize the size of the dots in scatter plot
+                    s=100, # customize the size of the dots in scatter plot
+                    label="Class A") # customize the label name of the first scatter 
 
 # the second scatter plot
 plt.scatter(x2, y2, color="skyblue", # customize the color of the dots in scatter plot
                     alpha=0.5, # customize the transparency of the color of the dots in scatter plot
-                    s=100) # customize the size of the dots in scatter plot
+                    s=100, # customize the size of the dots in scatter plot
+                    label="Class B") # customize the label name of the second scatter 
 
 plt.title("Test Marks")
 plt.xlabel("Hours Studied")
 plt.ylabel("Grades")
+
+plt.legend() # we need this to show the legend which contains the names of the labels of the scatter plots to distinguish between the plots
 plt.show()
