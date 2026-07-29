397c901aaec71c8a8cdaf78cc492d90525e11d2e
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-29 08:56:44 +0200

    histograms

--- histograms.py
+++ histograms.py
@@ -2,3 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 # histogram is a graph that shows the distribution of numerical data.
+# Axes:
+# X-axis: ranges (intervals) of the data called bins.
+# Y-axis: the frequency (count) of values in each bin.
