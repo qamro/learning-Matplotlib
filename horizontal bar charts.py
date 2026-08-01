bdff1be04391ce6cace32ff6ad3b0c62bcb4371b
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-01 07:16:20 +0200

    horizontal bar charts

--- horizontal bar charts.py
+++ horizontal bar charts.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np         
 
 #NOTE: i used a normal list for categories cuz a list of strings doesn't benefit from numpy arrays
-categories = ["grain", "fruits", "proteins", "sweets"]
-values = np.array([3, 5, 11, 4])
+categories = ["grain", "fruits", "vegetables", "proteins", "sweets"]
+values = np.array([3, 5, 2, 11, 4])
 
 # create our horizontal bar chart and customize it as we want
-# NOTE: we can customize our horizontal bar charts as we want just like we did with the line plots
+# NOTE: wa can customize our horizontal bar charts as we want just like we did with the the normal bar charts
 plt.barh(categories, values, color="skyblue")
 plt.title("Daily Consumption")
 plt.xlabel("Quantity")
+plt.ylabel("Food")
+
+plt.tight_layout() # adjust the spacing and layout of the plot to prevent overlap and make it look better
 plt.show()
