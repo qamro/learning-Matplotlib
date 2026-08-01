e522905fd6413fe4659fb5370bbe2bf4a336713a
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-01 06:17:17 +0200

    subplots

--- subplots.py
+++ subplots.py
@@ -1,3 +1,12 @@
 import matplotlib.pyplot as plt       
 import numpy as np 
 
+
+# NOTE: we can use the subplots() function to create multiple plots in the same figure
+# NOTE: the subplots() function returns two values: the figure and the axes of the subplots we created
+# NOTE: the axes is a 2D array of the subplots we created, so we can access each subplot by its index in the array
+# NOTE: the subplots() function takes two arguments: the number of rows and the number of columns of the subplots we want to create
+
+
+# to create subplots we can use the plt.subplots() function which takes two arguments:
+# the number of rows and the number of columns of the subplots we want to create
