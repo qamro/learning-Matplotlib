73042512eae3acef3791ee70bfe628a8d5b22f53
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-01 06:45:43 +0200

    set up the df

--- visualisation with pandas.py
+++ visualisation with pandas.py
@@ -2,3 +2,5 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd   
 
+df = pd.read_csv("students.csv") # read the csv file and store it in a pandas dataframe
+print(df) # print the dataframe to see what it looks like
