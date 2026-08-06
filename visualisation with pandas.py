ad5f4c3259ba5572004b5e4ae5182f6f49c57484
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-06 23:24:48 +0200

    visualisation with pandas

--- visualisation with pandas.py
+++ visualisation with pandas.py
@@ -6,7 +6,7 @@ df = pd.read_csv("students.csv") # read the csv file and store it in a pandas da
 print(df) # print the dataframe to see what it looks like
 print()
 
-# NOTE: we can use the value_counts() function to count the number of students in each age group and store it in a variable
+# NOTE: we can use the value_counts() function to count the number of students in each City group and store it in a variable
 # NOTE: the value_counts() function returns a pandas series with: index = "unique values" and, values = "number of times each value appears"
 city_count = df["City"].value_counts() # count the number of students in each city and store it in a variable
 print(city_count) # print the city count to see what it looks like
